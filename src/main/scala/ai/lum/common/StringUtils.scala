43eeaec7c2fdfbaf36d3e63d2091dd9947c09303
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2016-07-30 15:07:56 -0700

    comments

--- src/main/scala/ai/lum/common/StringUtils.scala
+++ src/main/scala/ai/lum/common/StringUtils.scala
@@ -29,7 +29,10 @@ object StringUtils {
     /** Removes diacritics from a string. */
     def stripAccents: String = ApacheStringUtils.stripAccents(str)
 
-    /** Returns a String value for a CSV column enclosed in double quotes, if required. */
+    /**
+     * Returns a String value for a CSV column enclosed in double quotes,
+     * if required.
+     */
     def escapeCsv: String = StringEscapeUtils.escapeCsv(str)
 
     /** Returns a String value for an unescaped CSV column. */
@@ -38,7 +41,10 @@ object StringUtils {
     /** Escapes the characters in a String using HTML entities. */
     def escapeHtml: String = StringEscapeUtils.escapeHtml4(str)
 
-    /** Unescapes a string containing entity escapes to a string containing the actual Unicode characters corresponding to the escapes. */
+    /**
+     * Unescapes a string containing entity escapes to a string containing
+     * the actual Unicode characters corresponding to the escapes.
+     */
     def unescapeHtml: String = StringEscapeUtils.unescapeHtml4(str)
 
     /** Escapes the characters in a String using Java String rules. */
@@ -56,13 +62,20 @@ object StringUtils {
     /** Escapes the characters in a String using XML entities. */
     def escapeXml: String = StringEscapeUtils.escapeXml10(str)
 
-    /** Unescapes a string containing XML entity escapes to a string containing the actual Unicode characters corresponding to the escapes. */
+    /**
+     * Unescapes a string containing XML entity escapes to a string
+     * containing the actual Unicode characters corresponding to the
+     * escapes.
+     */
     def unescapeXml: String = StringEscapeUtils.unescapeXml(str)
 
     /** Splits the provided text on whitespace. */
     def splitOnWhitespace: Array[String] = ApacheStringUtils.split(str)
 
-    /** Removes leading and trailing whitespace and replaces sequences of whitespace characters by a single space. */
+    /**
+     * Removes leading and trailing whitespace and replaces sequences of
+     * whitespace characters by a single space.
+     */
     def normalizeSpace: String = ApacheStringUtils.normalizeSpace(str)
 
     def normalize: String = normalizeSpace.stripAccents.toLowerCase
@@ -71,7 +84,10 @@ object StringUtils {
 
     def titleCase: String = WordUtils.capitalizeFully(str)
 
-    /** Swaps the case of a String changing upper and title case to lower case, and lower case to upper case. */
+    /**
+     * Swaps the case of a String changing upper and title case to
+     * lower case, and lower case to upper case.
+     */
     def swapCase: String = ApacheStringUtils.swapCase(str)
 
     /** Abbreviates a String using ellipses. */
