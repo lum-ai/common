f0d2c7ade8430515de8e122e40ddc87bf6cb0e3b
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2016-11-16 23:14:26 -0700

    par iterator

---
+++ src/main/scala/ai/lum/common/IteratorUtils.scala
@@ -0,0 +1,81 @@
+package ai.lum.common
+
+import scala.collection.{ AbstractIterator, GenTraversableOnce }
+import scala.collection.parallel.{ ParSeq, TaskSupport, defaultTaskSupport }
+
+object IteratorUtils {
+
+  implicit class IteratorWrapper[A](val iterator: Iterator[A]) extends AnyVal {
+    def par: ParIterator[A] = parGroups(1024) // is there a better number?
+    def parGroups(n: Int): ParIterator[A] = new ParIterator(iterator.grouped(n))
+  }
+
+  // Gets an iterator of groups of A.
+  // Each group is traversed in parallel.
+  class ParIterator[A](
+      private val remainingGroups: Iterator[Seq[A]]
+  ) extends AbstractIterator[A] { self =>
+
+    var tasksupport: TaskSupport = defaultTaskSupport
+
+    private var currentGroup: List[A] = Nil
+
+    def next(): A = currentGroup match {
+      case head :: tail =>
+        // return next element in current group
+        currentGroup = tail
+        head
+      case Nil =>
+        // get next group and try again
+        currentGroup = remainingGroups.next().toList
+        next()
+    }
+
+    def hasNext: Boolean = currentGroup.nonEmpty || remainingGroups.hasNext
+
+    // returns sequential version of this iterator
+    override def seq: Iterator[A] = new AbstractIterator[A] {
+      def next(): A = self.next()
+      def hasNext: Boolean = self.hasNext
+    }
+
+    // parallelizes a single group and attaches tasksupport
+    private def mkPar(group: Seq[A]): ParSeq[A] = {
+      val par = group.par
+      par.tasksupport = tasksupport
+      par
+    }
+
+    private def allGroups: Iterator[Seq[A]] = {
+      if (currentGroup.isEmpty) {
+        remainingGroups
+      } else {
+        Iterator(currentGroup) ++ remainingGroups
+      }
+    }
+
+    override def foreach[U](f: A => U): Unit = {
+      allGroups.foreach(g => mkPar(g).foreach(f))
+    }
+
+    override def map[B](f: A => B): Iterator[B] = {
+      val parIter = new ParIterator(allGroups.map(g => mkPar(g).map(f).seq))
+      parIter.tasksupport = tasksupport
+      parIter
+    }
+
+    override def flatMap[B](f: A => GenTraversableOnce[B]): Iterator[B] = {
+      val parIter = new ParIterator(allGroups.map(g => mkPar(g).flatMap(f).seq))
+      parIter.tasksupport = tasksupport
+      parIter
+    }
+
+    override def filter(p: A => Boolean): Iterator[A] = {
+      val parIter = new ParIterator(allGroups.map(g => mkPar(g).filter(p).seq))
+      parIter.tasksupport = tasksupport
+      parIter
+    }
+
+  }
+
+}
