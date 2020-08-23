d8eee64239b46ac73643ea0d706ac61cf753aae0
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2020-08-23 16:21:24 -0700

    tests

--- src/test/scala/ai/lum/common/TestTryWithResources.scala
+++ src/test/scala/ai/lum/common/TestTryWithResources.scala
@@ -8,51 +8,84 @@ class TestTryWithResources extends FlatSpec with Matchers {
   import TestTryWithResources._
 
   "TryWithResources" should "not throw an exception" in {
-    noException should be thrownBy using (new Closeable) { c =>
+    val closeable = new Closeable
+    noException should be thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    closeable.n should be (1)
   }
 
   it should "throw an exception" in {
-    val thrown = the [Exception] thrownBy using (new CloseableWithException) { c =>
+    val closeable = new CloseableWithException
+    val thrown = the [Exception] thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    thrown.getMessage() shouldEqual ("doSomething")
     thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
   }
 
   it should "throw a fatal exception" in {
-    val thrown = the [InterruptedException] thrownBy using (new CloseableWithFatalException) { c =>
+    val closeable = new CloseableWithFatalException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    thrown.getMessage() shouldEqual ("doSomething")
     thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
   }
 
   it should "throw an exception during closing" in {
-    val thrown = the [Exception] thrownBy using (new CloseableWithCloseException) { c =>
+    val closeable = new CloseableWithCloseException
+    val thrown = the [Exception] thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    thrown.getMessage() shouldEqual ("close")
     thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
   }
 
   it should "throw a fatal exception during closing" in {
-    val thrown = the [Exception] thrownBy using (new CloseableWithCloseFatalException) { c =>
+    val closeable = new CloseableWithCloseFatalException
+    val thrown = the [Exception] thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    thrown.getMessage() shouldEqual ("close")
     thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
   }
 
   it should "suppress exception when closing fatal exception occurs" in {
-    val thrown = the [InterruptedException] thrownBy using (new CloseableWithExceptionAndCloseFatalException) { c =>
+    val closeable = new CloseableWithExceptionAndCloseFatalException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    thrown.getMessage() shouldEqual ("close")
     thrown.getSuppressed() should have length (1)
+    thrown.getSuppressed()(0).getMessage() shouldEqual ("doSomething")
+    closeable.n should be (1)
   }
 
   it should "suppress closing exception when fatal exception occurs" in {
-    val thrown = the [InterruptedException] thrownBy using (new CloseableWithExceptionAndCloseFatalException) { c =>
+    val closeable = new CloseableWithFatalExceptionAndCloseException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
       c.doSomething()
     }
+    thrown.getMessage() shouldEqual ("doSomething")
     thrown.getSuppressed() should have length (1)
+    thrown.getSuppressed()(0).getMessage() shouldEqual ("close")
+    closeable.n should be (1)
+  }
+
+  it should "suppress closing fatal exception when fatal exception occurs" in {
+    val closeable = new CloseableWithFatalExceptionAndCloseFatalException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("doSomething")
+    thrown.getSuppressed() should have length (1)
+    thrown.getSuppressed()(0).getMessage() shouldEqual ("close")
+    closeable.n should be (1)
   }
 
 }
@@ -60,53 +93,81 @@ class TestTryWithResources extends FlatSpec with Matchers {
 object TestTryWithResources {
 
   class Closeable {
-    def close(): Unit = {}
+    var n = 0
+    def close(): Unit = {
+      n += 1
+    }
     def doSomething(): Unit = {}
   }
 
   class CloseableWithException {
-    def close(): Unit = {}
+    var n = 0
+    def close(): Unit = {
+      n += 1
+    }
     def doSomething(): Unit = {
-      throw new Exception
+      throw new Exception("doSomething")
     }
   }
 
   class CloseableWithFatalException {
-    def close(): Unit = {}
+    var n = 0
+    def close(): Unit = {
+      n += 1
+    }
     def doSomething(): Unit = {
-      throw new InterruptedException
+      throw new InterruptedException("doSomething")
     }
   }
 
   class CloseableWithCloseException {
+    var n = 0
     def close(): Unit = {
-      throw new Exception
+      n += 1
+      throw new Exception("close")
     }
     def doSomething(): Unit = {}
   }
 
   class CloseableWithCloseFatalException {
+    var n = 0
     def close(): Unit = {
-      throw new InterruptedException
+      n += 1
+      throw new InterruptedException("close")
     }
     def doSomething(): Unit = {}
   }
 
   class CloseableWithFatalExceptionAndCloseException {
+    var n = 0
     def close(): Unit = {
-      throw new Exception
+      n += 1
+      throw new Exception("close")
     }
     def doSomething(): Unit = {
-      throw new InterruptedException
+      throw new InterruptedException("doSomething")
     }
   }
 
   class CloseableWithExceptionAndCloseFatalException {
+    var n = 0
     def close(): Unit = {
-      throw new InterruptedException
+      n += 1
+      throw new InterruptedException("close")
     }
     def doSomething(): Unit = {
-      throw new Exception
+      throw new Exception("doSomething")
+    }
+  }
+
+  class CloseableWithFatalExceptionAndCloseFatalException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+      throw new InterruptedException("close")
+    }
+    def doSomething(): Unit = {
+      throw new InterruptedException("doSomething")
     }
   }
 
