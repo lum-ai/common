d6f6af40c597823bfde37bfbadf59242ec1f8aa1
marcovzla <marcos.vzla@gmail.com>
2020-08-23 16:22:40 -0700

    don't close twice (#26)
    * don't close twice
    
    * tests
    
    * tests

--- src/main/scala/ai/lum/common/TryWithResources.scala
+++ src/main/scala/ai/lum/common/TryWithResources.scala
@@ -25,22 +25,48 @@ object TryWithResources {
 
   /** like java's try-with-resources */
   def using[A <: Closeable, B](resource: A)(f: A => B): B = {
+    var exception: Option[Throwable] = None
     try {
+      // try to call f() with the resource as argument
       f(resource)
     } catch {
-      case e1: Throwable =>
-        try {
-          resource.close()
-        } catch {
-          case NonFatal(e2) =>
-            e1.addSuppressed(e2)
-          case e2: Throwable =>
-            e2.addSuppressed(e1)
-            throw e2
-        }
-        throw e1
+      case e: Throwable =>
+        // if there is an exception
+        // then save it for later and rethrow it
+        exception = Some(e)
+        throw e
     } finally {
-      resource.close()
+      exception match {
+        case None =>
+          // if there was no exception
+          // then we can just close the resource
+          // and if there is an exception during closing
+          // we can just let it happen
+          resource.close()
+        case Some(e1) =>
+          // if there was an exception
+          // then we need to be more careful when closing the resource
+          try {
+            resource.close()
+          } catch {
+            case NonFatal(e2) =>
+              // if we get a nonfatal exception during closing
+              // then we need to suppress it
+              e1.addSuppressed(e2)
+            case e2: Throwable if NonFatal(e1) =>
+              // if we get a fatal exception during closing
+              // and the original exception wasn't fatal
+              // then we need to suppress the original exception
+              // and throw the new one
+              e2.addSuppressed(e1)
+              throw e2
+            case e2: Throwable =>
+              // if we get a fatal exception during closing
+              // and the original exception was fatal too
+              // then we suppress the new exception
+              e1.addSuppressed(e2)
+          }
+      }
     }
   }
 
---
+++ src/test/scala/ai/lum/common/TestTryWithResources.scala
@@ -0,0 +1,174 @@
+package ai.lum.common
+
+import org.scalatest._
+import TryWithResources.using
+
+class TestTryWithResources extends FlatSpec with Matchers {
+
+  import TestTryWithResources._
+
+  "TryWithResources" should "not throw an exception" in {
+    val closeable = new Closeable
+    noException should be thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    closeable.n should be (1)
+  }
+
+  it should "throw an exception" in {
+    val closeable = new CloseableWithException
+    val thrown = the [Exception] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("doSomething")
+    thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
+  }
+
+  it should "throw a fatal exception" in {
+    val closeable = new CloseableWithFatalException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("doSomething")
+    thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
+  }
+
+  it should "throw an exception during closing" in {
+    val closeable = new CloseableWithCloseException
+    val thrown = the [Exception] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("close")
+    thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
+  }
+
+  it should "throw a fatal exception during closing" in {
+    val closeable = new CloseableWithCloseFatalException
+    val thrown = the [Exception] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("close")
+    thrown.getSuppressed() should have length (0)
+    closeable.n should be (1)
+  }
+
+  it should "suppress exception when closing fatal exception occurs" in {
+    val closeable = new CloseableWithExceptionAndCloseFatalException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("close")
+    thrown.getSuppressed() should have length (1)
+    thrown.getSuppressed()(0).getMessage() shouldEqual ("doSomething")
+    closeable.n should be (1)
+  }
+
+  it should "suppress closing exception when fatal exception occurs" in {
+    val closeable = new CloseableWithFatalExceptionAndCloseException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("doSomething")
+    thrown.getSuppressed() should have length (1)
+    thrown.getSuppressed()(0).getMessage() shouldEqual ("close")
+    closeable.n should be (1)
+  }
+
+  it should "suppress closing fatal exception when fatal exception occurs" in {
+    val closeable = new CloseableWithFatalExceptionAndCloseFatalException
+    val thrown = the [InterruptedException] thrownBy using (closeable) { c =>
+      c.doSomething()
+    }
+    thrown.getMessage() shouldEqual ("doSomething")
+    thrown.getSuppressed() should have length (1)
+    thrown.getSuppressed()(0).getMessage() shouldEqual ("close")
+    closeable.n should be (1)
+  }
+
+}
+
+object TestTryWithResources {
+
+  class Closeable {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+    }
+    def doSomething(): Unit = {}
+  }
+
+  class CloseableWithException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+    }
+    def doSomething(): Unit = {
+      throw new Exception("doSomething")
+    }
+  }
+
+  class CloseableWithFatalException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+    }
+    def doSomething(): Unit = {
+      throw new InterruptedException("doSomething")
+    }
+  }
+
+  class CloseableWithCloseException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+      throw new Exception("close")
+    }
+    def doSomething(): Unit = {}
+  }
+
+  class CloseableWithCloseFatalException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+      throw new InterruptedException("close")
+    }
+    def doSomething(): Unit = {}
+  }
+
+  class CloseableWithFatalExceptionAndCloseException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+      throw new Exception("close")
+    }
+    def doSomething(): Unit = {
+      throw new InterruptedException("doSomething")
+    }
+  }
+
+  class CloseableWithExceptionAndCloseFatalException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+      throw new InterruptedException("close")
+    }
+    def doSomething(): Unit = {
+      throw new Exception("doSomething")
+    }
+  }
+
+  class CloseableWithFatalExceptionAndCloseFatalException {
+    var n = 0
+    def close(): Unit = {
+      n += 1
+      throw new InterruptedException("close")
+    }
+    def doSomething(): Unit = {
+      throw new InterruptedException("doSomething")
+    }
+  }
+
+}
